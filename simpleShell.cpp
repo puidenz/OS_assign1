8659a959bdeafa39e89cb95e17d09eab98f50385
Purin <dglpw7546@gmail.com>
2018-10-15 02:24:35 +0800

    commit -m

--- simpleShell.cpp
+++ simpleShell.cpp
@@ -11,9 +11,14 @@
 #include<sys/stat.h>
 #include<fcntl.h>
 
+#define back_ground 0
+#define redirection 1
+#define pipe_flag 2
+
 using namespace std;
 
 int parse_argv(vector<string> &in_command, char** &argv);
+int process_run(bool flag[], int argc, char** argv, string filename, int input_from);
 void sig_handle(int sig);
 
 int main()
@@ -22,13 +27,10 @@ int main()
     while(true){
         string input;       //store the input line
         stringstream ss;    //stringstream for string handling
-        
         char **myArgv;      //arguments array
         int myArgc;         //number of arguments
         string filename;	//store filename for redirection
-		bool back_ground = false;          				 //flag for idicatiing if back_ground mode "&"
-        bool redirection = false;
-		
+        bool flag[3] = {false, false, false};           //flag for idicatiing if back_ground mode "&"
 
 		cout << ">";
 		getline(cin, input);
@@ -40,57 +42,60 @@ int main()
 
         while(ss >> tmp_s){
 			if(tmp_s == ">"){
-				redirection = true;
+				flag[redirection] = true;
 				ss >> filename;
 			}
+            else if(tmp_s == "|"){
+                flag[pipe_flag] = true;
+            }
             else
                 tmp.push_back(tmp_s);
         }
 
+
         myArgc = parse_argv(tmp, myArgv);    			//get argument!
         if(strcmp(myArgv[myArgc-1], "&") == 0){			//check if & exit
-            back_ground = true;
+            flag[back_ground] = true;
             myArgv[myArgc-1] = NULL;
         }
-        pid_t pid = fork();
 
-
-        if(pid < 0)
-            cout << "Fork process error!" << endl;
+        process_run(flag, myArgc, myArgv, filename, 0);
+        // pid_t pid = fork();
+        // if(pid < 0)
+        //     cout << "Fork process error!" << endl;
         
-        else if(pid == 0){ 					 //child process
-            int fd = 0;
+        // else if(pid == 0){ 					 //child process
+        //     int fd = 0;
             
-            if(redirection == true){
-                fd = open(filename.c_str(), O_RDWR);
+        //     if(redirection == true){
+        //         fd = open(filename.c_str(), O_RDWR);
 
-                if(fd < 0){                 //check if correctly open file
-                    cout << "open file error!" << endl;
-                    exit(-1);
-                }
+        //         if(fd < 0){                 //check if correctly open file
+        //             cout << "open file error!" << endl;
+        //             exit(-1);
+        //         }
                 
-                close(1);
-                dup2(fd, 1);
-                close(fd);
-            }
+        //         close(1);
+        //         dup2(fd, 1);
+        //         close(fd);
+        //     }
             
 
-            execvp(myArgv[0], myArgv);
+        //     execvp(myArgv[0], myArgv);
             
-            exit(0);
-        }
-		
-        else{								 //parent process
-            if(back_ground == false)         //check is necessary to wait child
-                wait(&pid);
-            else
-                signal(SIGCHLD, sig_handle);
+        //     exit(0);
+        // }
+        // else{								 //parent process
+        //     if(back_ground == false)         //check is necessary to wait child
+        //         wait(&pid);
+        //     else
+        //         signal(SIGCHLD, sig_handle);
             
-                                             //Free memory!!
-            for(int i=0; i<=myArgc; i++)     //size of myArgv is myArgc+1
-                delete[] myArgv[i];
-            delete[] myArgv;
-        }
+        //                                      //Free memory!!
+        //     for(int i=0; i<=myArgc; i++)     //size of myArgv is myArgc+1
+        //         delete[] myArgv[i];
+        //     delete[] myArgv;
+        // }
     }
     return 0;
 }
@@ -109,4 +114,40 @@ int parse_argv(vector<string> &in_command, char** &argv){
 void sig_handle(int sig){
     int status;
     while(waitpid(-1, &status, WNOHANG) > 0);
+}
+
+int process_run(bool flag[], int argc, char** argv, string filename, int input_from){
+    pid_t pid = fork();
+
+    if(pid < 0)
+        cout << "Fork process error!" << endl;
+    
+    else if(pid == 0){ 					 //child process
+        int fd = 0;
+        
+        if(flag[redirection] == true){
+            fd = open(filename.c_str(), O_RDWR);
+
+            if(fd < 0){                 //check if correctly open file
+                cout << "open file error!" << endl;
+                exit(-1);
+            }
+            
+            dup2(fd, 1);
+            close(fd);
+        }
+        
+        execvp(argv[0], argv);
+    }
+    else{								 //parent process
+        if(flag[back_ground] == false)         //check is necessary to wait child
+            wait(&pid);
+        else
+            signal(SIGCHLD, sig_handle);
+        
+                                         //Free memory!!
+        for(int i=0; i<=argc; i++)       //size of argv is argc+1
+            delete[] argv[i];
+        delete[] argv;
+    }
 }
